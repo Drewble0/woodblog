be79a8639094d65afb87ea595506b6749021b952
Andrew Francis <afrancis@welltower.com>
2019-07-31 15:37:59 -0400

    Initial Commit

---
+++ content/blog/real-post/index.md
@@ -0,0 +1,5 @@
+---
+title: Real Post
+---
+
+This is the first real blog post created for Gatsby.
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Starter Blog`,
+    author: `Andrew Francis`,
+    description: `A first attmept at using Gatsby for a blog.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `kylemathews`,
